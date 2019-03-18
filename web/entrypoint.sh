895baec364fe9cad5b855e4eaaafdc0b0c9ea596
Timo Halbesma <timohalbesma@gmail.com>
2019-03-18 20:16:27 +0100

    Steal Dockerized setup of Mattermost from mattermost/mattermost-docker. No shame.

---
+++ web/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Define default value for app container hostname and port
+MATTERMOST_HOST=${MATTERMOST_HOST:-mattermost}
+MATTERMOST_PORT_NUMBER=${MATTERMOST_PORT_NUMBER:-8000}
+
+# Check if SSL should be enabled (if certificates exists)
+if [ -f "/cert/cert.pem" -a -f "/cert/key-no-password.pem" ]; then
+  echo "found certificate and key, linking ssl config"
+  ssl="-ssl"
+else
+  echo "linking plain config"
+fi
+# Linking Nginx configuration file
+ln -s /etc/nginx/sites-available/mattermost$ssl /etc/nginx/conf.d/mattermost.conf
+
+# Setup app host and port on configuration file
+sed -i "s/{%MATTERMOST_HOST%}/${MATTERMOST_HOST}/g" /etc/nginx/conf.d/mattermost.conf
+sed -i "s/{%MATTERMOST_PORT%}/${MATTERMOST_PORT_NUMBER}/g" /etc/nginx/conf.d/mattermost.conf
+
+# Run Nginx
+exec nginx -g 'daemon off;'
