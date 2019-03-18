fed5484b60d9be3deaac5fa853df6ad2949620cf
Timo Halbesma <timohalbesma@gmail.com>
2019-03-18 22:38:15 +0100

    First steps to add certbot to nginx [untested]

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -12,11 +12,14 @@ else
   echo "linking plain config"
 fi
 # Linking Nginx configuration file
-ln -s /etc/nginx/sites-available/mattermost$ssl /etc/nginx/conf.d/mattermost.conf
+# ln -s /etc/nginx/sites-available/mattermost$ssl /etc/nginx/conf.d/mattermost.conf
+ln -s /etc/nginx/sites-available/mancelot.nl.conf /etc/nginx/conf.d
 
 # Setup app host and port on configuration file
-sed -i "s/{%MATTERMOST_HOST%}/${MATTERMOST_HOST}/g" /etc/nginx/conf.d/mattermost.conf
-sed -i "s/{%MATTERMOST_PORT%}/${MATTERMOST_PORT_NUMBER}/g" /etc/nginx/conf.d/mattermost.conf
+# sed -i "s/{%MATTERMOST_HOST%}/${MATTERMOST_HOST}/g" /etc/nginx/conf.d/mattermost.conf
+# sed -i "s/{%MATTERMOST_PORT%}/${MATTERMOST_PORT_NUMBER}/g" /etc/nginx/conf.d/mattermost.conf
 
 # Run Nginx
+# TODO? command: "/bin/sh -c 'while :; do sleep 6h & wait $${!}; nginx -s reload; done & nginx -g \"daemon off;\"'"
 exec nginx -g 'daemon off;'
+
