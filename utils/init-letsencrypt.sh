b363c582acf463e03aa49403ebb40548ee8dece3
Timo Halbesma <timohalbesma@gmail.com>
2019-03-28 13:38:01 +0100

    Loop over each (sub)domain we want to Let's Encrypt

--- utils/init-letsencrypt.sh
+++ utils/init-letsencrypt.sh
@@ -2,20 +2,17 @@
 
 set -e
 
-DOMAINS=(mancelot.nl www.mancelot.nl)
-RSA_KEY_SIZE=4096
 DATA_PATH="./data/certbot"
-EMAIL="info@mancelot.nl" # Adding a valid address is strongly recommended
-STAGING=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+EMAIL="info@mancelot.nl" 
+RSA_KEY_SIZE=4096
 
-if [ -d "${DATA_PATH}" ]; then
-  read -p "Existing data found for $DOMAINS. Continue and replace existing certificate? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
-  fi
-fi
+# Select appropriate EMAIL arg
+case "$EMAIL" in
+  "") EMAIL_ARG="--register-unsafely-without-email" ;;
+  *) EMAIL_ARG="--email $EMAIL" ;;
+esac
 
-
+# First we obtain options-ssl-nginx.conf and ssl-dhparams.pem from certbot repo
 if [ ! -e "${DATA_PATH}/conf/options-ssl-nginx.conf" ] || [ ! -e "${DATA_PATH}/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
   mkdir -p "${DATA_PATH}/conf"
@@ -24,55 +21,72 @@ if [ ! -e "${DATA_PATH}/conf/options-ssl-nginx.conf" ] || [ ! -e "${DATA_PATH}/c
   echo
 fi
 
-echo "### Creating dummy certificate for $DOMAINS ..."
-LE_PATH="/etc/letsencrypt/live/$DOMAINS"
-mkdir -p "${DATA_PATH}/conf/live/$DOMAINS"  # Inside the container
-docker-compose run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:1024 -days 1\
-    -keyout '${LE_PATH}/privkey.pem' \
-    -out '${LE_PATH}/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
-echo
+# Then we declare each of our domains
+declare -a DOMAINS=(
+    "mancelot.be"
+    "mancelot.com"
+    "mancelot.co.uk"
+    "mancelot.de"
+    "mancelot.eu"
+    "mancelot.nl"
+        "mm.mancelot.nl"
+)
 
+for DOMAIN in "${DOMAINS[@]}"
+do
+    echo "### Creating dummy certificate for $DOMAIN ..."
+    LE_PATH="/etc/letsencrypt/live/$DOMAIN"
+    mkdir -p "${DATA_PATH}/conf/live/$DOMAIN"  # Inside the container
+    docker-compose run --rm --entrypoint "\
+      openssl req -x509 -nodes -newkey rsa:1024 -days 1\
+        -keyout '${LE_PATH}/privkey.pem' \
+        -out '${LE_PATH}/fullchain.pem' \
+        -subj '/CN=localhost'" certbot
+    echo
+
+    is_subdomain=false
+    echo -e "\nDomain: $DOMAIN"
+    ndots_when_subdomain=2
+    if [[ $DOMAIN =~ ".co.uk" ]]; then
+        ndots_when_subdomain=3
+    fi
+
+    dots="${DOMAIN//[^.]}"
+    ndots=${#dots}
+    if (( $ndots >= $ndots_when_subdomain )); then
+        is_subdomain=true
+    fi
+
+    if $is_subdomain; then
+        DOMAIN_ARGS="-d ${DOMAIN}"
+    else
+        DOMAIN_ARGS="-d ${DOMAIN} -d www.${DOMAIN}"
+        if [[ $DOMAIN =~ "projectcece" ]]; then
+            DOMAIN_ARGS="${DOMAIN_ARGS} -d staging.${DOMAIN}"
+        fi
+    fi
+
+    echo "### Deleting dummy certificate for $DOMAIN ..."
+    docker-compose run --rm --entrypoint "\
+      rm -Rf /etc/letsencrypt/live/$DOMAIN && \
+      rm -Rf /etc/letsencrypt/archive/$DOMAIN && \
+      rm -Rf /etc/letsencrypt/renewal/$DOMAIN.conf" certbot
+    echo
+    
+    echo "### Requesting Let's Encrypt certificate for $DOMAIN_ARGS ..."
+       
+    docker-compose run --rm --entrypoint "\
+      certbot certonly --webroot -w /var/www/certbot \
+        $EMAIL_ARG \
+        $DOMAIN_ARGS \
+        --rsa-key-size $RSA_KEY_SIZE \
+        --agree-tos \
+        --force-renewal" certbot
+    echo
+done
 
 echo "### Starting nginx ..."
 docker-compose up --force-recreate -d nginx
-echo
-
-exit 0
-
-echo "### Deleting dummy certificate for $DOMAINS ..."
-docker-compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$DOMAINS && \
-  rm -Rf /etc/letsencrypt/archive/$DOMAINS && \
-  rm -Rf /etc/letsencrypt/renewal/$DOMAINS.conf" certbot
-echo
-
-
-echo "### Requesting Let's Encrypt certificate for $DOMAINS ..."
-#Join $DOMAINS to -d args
-DOMAIN_ARGS=""
-for domain in "${DOMAINS[@]}"; do
-  DOMAIN_ARGS="$DOMAIN_ARGS -d $domain"
-done
-
-# Select appropriate EMAIL arg
-case "$EMAIL" in
-  "") EMAIL_ARG="--register-unsafely-without-email" ;;
-  *) EMAIL_ARG="--email $EMAIL" ;;
-esac
-
-# Enable staging mode if needed
-if [ ${STAGING} != "0" ]; then STAGING_ARG="--staging --dry-run"; fi
-
-docker-compose run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
-    $STAGING_ARG \
-    $EMAIL_ARG \
-    $DOMAIN_ARGS \
-    --rsa-key-size $RSA_KEY_SIZE \
-    --agree-tos \
-    --force-renewal" certbot
 echo
 
 echo "### Reloading nginx ..."
