37d272de12cda0343682d93ab33293cc2537f379
Timo Halbesma <timohalbesma@gmail.com>
2019-04-30 17:32:42 +0200

    Use the admin:login instead of the on-site login. Fixes MANCELOT-BACKEND-4

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -136,7 +136,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 AUTH_USER_MODEL = "accounts.UserModel"
-LOGIN_URL = "accounts:login"
+LOGIN_URL = "admin:login"
 LOGIN_REDIRECT_URL = "accounts:profile"
 ADMIN_BCC = []
 
