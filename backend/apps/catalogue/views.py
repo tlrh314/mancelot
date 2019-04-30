e2025e6362d07a0ce32e4d15ec169c6e4b3ee751
Timo Halbesma <timohalbesma@gmail.com>
2019-04-30 17:27:42 +0200

    More graceful handling of 500

---
+++ backend/apps/catalogue/views.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.shortcuts import render
+
+
+def handler404(request, exception=None, template_name=None):
+    from sentry_sdk import capture_message
+    # capture_message("Page not found!", level="error")
+
+    return render(request, "404.html", { "request_path": request.path,
+        "exception": exception.__class__.__name__ } )
+
+
+def handler500(request, *args, **argv):
+    from sentry_sdk import last_event_id
+
+    return render(request, "500.html", {
+        "sentry_event_id": last_event_id(),
+        "sentry_dsn": settings.SENTRY_DSN_API
+    }, status=500)
