39ff3cbe0df6daee54a911f3c626d97a5d077e8b
Lorentz Stout <lorentz@aptic.nl>
2019-06-10 22:50:07 +0200

    better styling for homepage

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,11 +1,21 @@
 import { h } from 'preact';
 import style from './style';
 
+//TODO: later mannetje in delen opsplitsen zodat shirt los kan animeren, lange broek en jas vouwen uit.
 const Home = () => (
 	<div class={style.home}>
 		<div class="header_home"></div>
 		<h1>Waar ben je naar op zoek?</h1>
-		<div class="category_man"></div>
+		<div class="home_category_container">
+
+			<div class="category_label shirt"><span>Shirt</span><span class="small">en overhemd</span><div class="line"></div></div>
+			<div class="category_label jack"><span>Jas, trui</span><span class="small">en colbert</span><div class="line"></div></div>
+			<div class="category_label short"><span>Korte broek</span><span class="small">en onderbroek</span><div class="line"></div></div>
+			<div class="category_label long"><span>Lange broek</span><div class="line"></div></div>
+			<div class="category_label sock"><span>Sokken</span><div class="line"></div></div>
+			<div class="category_label shoe"><span>Schoenen</span><div class="line"></div></div>
+		</div>
+
 		<div class="footer">
 			<div class="btn_settings"></div>
 			<div class="btn_info"></div>
