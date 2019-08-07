e6fc8d1a4eeaf618d03fb19bede675dcbd67b8e7
Timo Halbesma <timohalbesma@gmail.com>
2019-08-07 02:36:21 +0200

    Various copy-pastes for additional tests

---
+++ backend/apps/accounts/tests/test_views.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.template.loader import render_to_string
+
+class LoginTestCase(TestCase):
+
+    def test_login_redirect_to_login_url(self):
+
+        # First check for the default behavior
+        response = self.client.get('/sekrit/')
+        self.assertRedirects(response, '/accounts/login/?next=/sekrit/')
+
+        # Then override the LOGIN_URL setting
+        with self.settings(LOGIN_URL='/other/login/'):
+            response = self.client.get('/sekrit/')
+            self.assertRedirects(response, '/other/login/?next=/sekrit/')
+
+    def test_privacy_policy_is_rendered(self):
+        with self.assertTemplateUsed(template_name='index.html'):
+            render_to_string('index.html')
---
+++ backend/apps/catalogue/tests/test_views.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from catalogue.models import Brand
+from catalogue.models import Store
+from catalogue.models import Product
+from catalogue.models import Category
+from catalogue.models import SubCategory
+from catalogue.models import Certificate
+
+
+# self.assertIsNotNone(entry.get_absolute_url())
+
+
+# class IndexTests(TestCase):
+#
+#     def setUp(self):
+#         self.user = get_user_model().objects.create(
+#             email="timo@mancelot.nl", full_name="Timo Halbesma"
+#         )
+#
+#     def test_one_entry(self):
+#         Entry.objects.create(title='1-title', body='1-body', author=self.user)
+#         response = self.client.get('/')
+#         self.assertContains(response, '1-title')
+#         self.assertContains(response, '1-body')
+#
+#     def test_two_entries(self):
+#         Entry.objects.create(title='1-title', body='1-body', author=self.user)
+#         Entry.objects.create(title='2-title', body='2-body', author=self.user)
+#         response = self.client.get('/')
+#         self.assertContains(response, '1-title')
+#         self.assertContains(response, '1-body')
+#         self.assertContains(response, '2-title')
+
+#     def test_title_in_entry(self):
+#         response = self.client.get(self.entry.get_absolute_url())
+#         self.assertContains(response, self.entry.title)
+#
+#     def test_body_in_entry(self):
+#         response = self.client.get(self.entry.get_absolute_url())
+#         self.assertContains(response, self.entry.body)
+
