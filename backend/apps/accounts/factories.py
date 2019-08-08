7dd20f7f441ad0f2da6dede0f4e01e2254446282
Timo Halbesma <timohalbesma@gmail.com>
2019-08-08 22:23:29 +0200

    Implemented factories for the models in the accounts app #4

---
+++ backend/apps/accounts/factories.py
@@ -0,0 +1,54 @@
+import factory
+from faker import Factory
+from django.conf import settings
+from django_countries import countries
+
+from accounts.models import UserModel
+from catalogue.models import Product
+from catalogue.factories import ProductFactory
+
+faker = Factory.create("nl_NL")
+
+
+class UserModelFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = UserModel
+        django_get_or_create = ("email",)
+
+    email = factory.LazyAttribute(lambda _: faker.email())
+    full_name = factory.LazyAttribute(lambda _: faker.name())
+    address = factory.LazyAttribute(lambda _: faker.street_address())
+    zip_code = factory.LazyAttribute(lambda _: faker.postcode())
+    city = factory.LazyAttribute(lambda _: faker.city())
+    country = "NL"  # alternatively, use code below to randomly select
+    # country = factory.LazyAttribute(lambda _:
+    #     list(countries)[faker.random_int(min=0, max=len(countries)-1)][0]  # ('NL', 'Nederland')
+    # )
+
+    is_active = True
+    is_staff = False
+    is_superuser = False
+
+    @factory.post_generation
+    def favorites(self, create, extracted, **kwargs):  # M2M relation
+        if not create:
+            return
+
+        if extracted:
+            for product in extracted:
+                self.favorites.add(product)
+            return
+
+        # Create Product instances if there are less than one hundred available
+        if Product.objects.count() < 25:
+            ProductFactory.create_batch(25 - Product.objects.count())
+
+        number_of_favorites_to_add = faker.random_int(min=0, max=Product.objects.count()-1)
+        all_products_randomly_ordered = Product.objects.order_by("?")
+        for j in range(number_of_favorites_to_add):
+            self.favorites.add(all_products_randomly_ordered[j])
+
+
+class AdminFactory(UserModelFactory):
+    is_staff = True
+    is_superuser = True
