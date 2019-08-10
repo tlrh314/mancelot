b6213408c4fbaf704e69c3e5cc3784cb08b5d894
Timo Halbesma <timohalbesma@gmail.com>
2019-08-10 17:25:43 +0200

    Auto-register tasks /w Django application on load

--- backend/settings/__init__.py
+++ backend/settings/__init__.py
@@ -1,5 +1,5 @@
+from __future__ import absolute_import, unicode_literals
 from settings.base import *
-
 
 # NB, Load the filebrowser settings after base to ensure SECRET_KEY is set
 from filebrowser.sites import site
@@ -9,3 +9,9 @@ site.storage = FileSystemStorage(
     base_url=STATIC_URL
 )
 site.directory = "img/"
+
+
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from .celery import app as celery_app
+__all__ = ("celery_app",)
