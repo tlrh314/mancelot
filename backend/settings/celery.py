5f5c789cdbc35e1814a8fbd407023ff273d775ce
Timo Halbesma <timohalbesma@gmail.com>
2019-08-10 18:02:40 +0200

    Overwrite django-money default settings

--- backend/settings/celery.py
+++ backend/settings/celery.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import, unicode_literals
+
 import os
 import sys
 from celery import Celery
@@ -5,12 +7,12 @@ from celery import Celery
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Use the 'apps' folder for our project apps
-sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+# Use the "apps" folder for our project apps
+sys.path.insert(0, os.path.join(BASE_DIR, "apps"))
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-app = Celery('settings')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app = Celery("settings")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
