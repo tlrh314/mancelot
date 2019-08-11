7d8db04a691eb7a9ab6c1ed391c2fc4c1c7b79f7
Timo Halbesma <timohalbesma@gmail.com>
2019-08-11 11:03:04 +0200

    Requirements + tweaks for (faster) executing of tests

--- backend/manage.py
+++ backend/manage.py
@@ -2,8 +2,8 @@
 import os
 import sys
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -12,4 +12,47 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    if "test" in sys.argv:
+        import logging
+        from django.conf import settings
+        # logging.disable(logging.CRITICAL)
+        settings.DEBUG = False
+        settings.TEMPLATE_DEBUG = False
+        settings.PASSWORD_HASHERS = [
+            "django.contrib.auth.hashers.MD5PasswordHasher",
+        ]
+        # settings.DATABASES = {
+        #     "default": {
+        #         "ENGINE": "django.db.backends.sqlite3",
+        #         "NAME": "test_database",
+        #     }
+        # }
+        settings.MIDDLEWARE = [
+            "django.contrib.sessions.middleware.SessionMiddleware",
+            "django.middleware.locale.LocaleMiddleware",
+            "django.middleware.common.CommonMiddleware",
+            "django.middleware.csrf.CsrfViewMiddleware",
+            "django.contrib.auth.middleware.AuthenticationMiddleware",
+            "django.contrib.messages.middleware.MessageMiddleware",
+        ]
+        # from django.core.management import call_command
+        # call_command("migrate")
+    if "test" in sys.argv and "--time" in sys.argv:
+        sys.argv.remove("--time")
+        from django import test
+        import time
+
+        def setUp(self):
+            self.startTime = time.time()
+
+        def tearDown(self):
+            total = time.time() - self.startTime
+            if total > 0.5 or True:
+                print("\n\t\033[91m[{0}] took {1:.3f}s\033[0m".format(
+                    self._testMethodName, total))
+
+        test.TestCase.setUp = setUp
+        test.TestCase.tearDown = tearDown
+
     execute_from_command_line(sys.argv)
