29f3b8dbbcb07052eff2dacc5c22d6eafed9f988
Timo Halbesma <timohalbesma@gmail.com>
2019-09-08 23:24:38 +0200

    Filter options for the catalogue endpoints, closes #14

--- backend/apps/catalogue/api.py
+++ backend/apps/catalogue/api.py
@@ -43,84 +43,48 @@ class CeceLabelViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = CeceLabel.objects.order_by("name")
     serializer_class = CeceLabelSerializer
     permission_classes = [permissions.IsAuthenticated,]
-    filter_backends = [
-        filters.SearchFilter,
-        filters.OrderingFilter,
-        DjangoFilterBackend,
-    ]
-    filter_fields = ["name"]
-    ordering_fields = ["name",]
-    search_fields = ["name",]
 
 
 class CertificateViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Certificate.objects.order_by("name")
     serializer_class = CertificateSerializer
     permission_classes = [permissions.IsAuthenticated,]
-    filter_backends = [
-        filters.SearchFilter,
-        filters.OrderingFilter,
-        DjangoFilterBackend,
-    ]
-    filter_fields = ["name"]
-    ordering_fields = ["name",]
-    search_fields = ["name",]
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Category.objects.order_by("name")
+    queryset = Category.objects.prefetch_related("subcategories").order_by("name")
     serializer_class = CategorySerializer
     permission_classes = [permissions.IsAuthenticated,]
     filter_backends = [
-        filters.SearchFilter,
-        filters.OrderingFilter,
         DjangoFilterBackend,
     ]
-    filter_fields = ["name"]
-    ordering_fields = ["name",]
-    search_fields = ["name",]
+    filter_fields = ["section"]
 
 
 class SubcategoryViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Subcategory.objects.order_by("name")
+    queryset = Subcategory.objects.select_related("category").order_by("name")
     serializer_class = SubcategorySerializer
     permission_classes = [permissions.IsAuthenticated,]
     filter_backends = [
-        filters.SearchFilter,
-        filters.OrderingFilter,
         DjangoFilterBackend,
     ]
-    filter_fields = ["name"]
-    ordering_fields = ["name",]
-    search_fields = ["name",]
+    filter_fields = ["category__section"]
 
 
 class PaymentOptionViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = PaymentOption.objects.order_by("id")
     serializer_class = PaymentOptionSerializer
     permission_classes = [permissions.IsAuthenticated,]
-    filter_backends = [
-        filters.SearchFilter,
-        filters.OrderingFilter,
-        DjangoFilterBackend,
-    ]
-    filter_fields = ["name"]
-    ordering_fields = ["name",]
-    search_fields = ["name",]
 
 
 class StoreViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Store.objects.order_by("id")
+    queryset = Store.objects.prefetch_related("payment_options").order_by("id")
     serializer_class = StoreSerializer
     permission_classes = [permissions.IsAuthenticated,]
     filter_backends = [
-        filters.SearchFilter,
-        filters.OrderingFilter,
         DjangoFilterBackend,
     ]
-    filter_fields = ["name"]
-    ordering_fields = ["name",]
-    search_fields = ["name",]
+    filter_fields = ["payment_options",]
 
 
 class BrandViewSet(viewsets.ReadOnlyModelViewSet):
@@ -132,73 +96,45 @@ class BrandViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = BrandSerializer
     permission_classes = [permissions.IsAuthenticated,]
     filter_backends = [
-        filters.SearchFilter,
-        filters.OrderingFilter,
         DjangoFilterBackend,
     ]
-    filter_fields = ["name"]
-    ordering_fields = ["name",]
-    search_fields = ["name",]
+    filter_fields = ["labels", "certificates"]
 
 
 class SizeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Size.objects.order_by("id")
     serializer_class = SizeSerializer
     permission_classes = [permissions.IsAuthenticated,]
-    filter_backends = [
-        filters.SearchFilter,
-        filters.OrderingFilter,
-        DjangoFilterBackend,
-    ]
-    filter_fields = ["name"]
-    ordering_fields = ["name",]
-    search_fields = ["name",]
 
 
 class ColorViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Color.objects.order_by("id")
     serializer_class = SizeSerializer
     permission_classes = [permissions.IsAuthenticated,]
-    filter_backends = [
-        filters.SearchFilter,
-        filters.OrderingFilter,
-        DjangoFilterBackend,
-    ]
-    filter_fields = ["name"]
-    ordering_fields = ["name",]
-    search_fields = ["name",]
 
 
 class MaterialViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Material.objects.order_by("id")
     serializer_class = MaterialSerializer
     permission_classes = [permissions.IsAuthenticated,]
-    filter_backends = [
-        filters.SearchFilter,
-        filters.OrderingFilter,
-        DjangoFilterBackend,
-    ]
-    filter_fields = ["name"]
-    ordering_fields = ["name",]
-    search_fields = ["name",]
 
 
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Product.objects.order_by("id").select_related(
-        "brand"  # 1 query /w join
+        "brand",
     ).prefetch_related(
-        # 1 query /w join each, so 4 queries
-        "categories", "subcategories", "sizes", "colors",
+        "categories", "subcategories", "sizes", "colors",  # "materials",
+        "brand__labels", "brand__certificates"
     )
     permission_classes = [permissions.IsAuthenticated,]  # 1 query to usermodel
     filter_backends = [
         DjangoFilterBackend,
     ]
     filter_fields = [
-        # one query each, so 4 queries
-        "categories", "subcategories", "colors", "sizes",
+        "categories__section", "categories", "subcategories",
+        "brand", "brand__labels", "brand__certificates", "store",
+        "colors", "sizes",  # "materials",
     ]
-    # Total 10 queries + count --> 11 queries
 
     def get_serializer_class(self):
         if self.action == "list":
